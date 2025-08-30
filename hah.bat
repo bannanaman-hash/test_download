8323218a8397aa2d0dc9ac6d3b3d47eb25b0aeca
bannanaman-hash <useryt908987@gmail.com>
2025-08-29 19:41:34 -0700

    Create hah.bat
    hahahahahahahahahahaha

---
+++ hah.bat
@@ -0,0 +1,9 @@
+@echo off
+:: Ask for user input
+set /p userInput=Enter something: 
+
+:: Print what the user entered
+echo You entered: %userInput%
+
+:: Wait for user to press any key before exiting
+pause
